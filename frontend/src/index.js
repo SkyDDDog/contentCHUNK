0aad5830ce2330117230ce7e10c2ff275d9c98a9
zzzz7_ <zzzz7_@gitcode.net>
2024-05-04 21:15:50 +0800

    test(frontend):add test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+/* 添加注释 */
 // 1. Import the extendTheme function
 import { extendTheme } from '@chakra-ui/react';
 
