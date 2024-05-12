6351d42a773504b017592013534814a624929977
yuweikun <742750246@qq.com>
2024-05-12 23:16:32 +0800

    添加对zhipu的支持和异步

--- AIbackend/server/chat/base_chat.py
+++ AIbackend/server/chat/base_chat.py
@@ -1,9 +1,14 @@
 import os
+import sys
 from langchain_community.chat_models.moonshot import MoonshotChat
 from langchain_core.messages import HumanMessage, SystemMessage
-#API KEY
-os.environ["MOONSHOT_API_KEY"] = "sk-H42Y6H7VvQbYxk26csvGosoeg6RBTq7LzxQdaQC0somYg1Ql"
+import asyncio
+from langchain_core.prompts import ChatPromptTemplate
+from AIbackend.model.init_llm import init_llm
 
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+
+#from model import init_llm
 
 messages = [
     SystemMessage(
@@ -14,21 +19,26 @@ messages = [
     ),
 ]
 
-# print(output)
-# async def chat1():
-#     for chunk in chat.astream(messages) :
-#         print(chunk.content, end="|", flush=True)
+messages1 = [
+    SystemMessage(
+        content="You are a helpful assistant that translates English to French."
+    ),
+    HumanMessage(
+        content="给我输出一篇小红书文案，主题是键盘"
+    ),
+]
 
-# asyncio.run(chat1())
 
-def base_chat(messages) :
-    chat = MoonshotChat(streaming=True)
-    output= chat.stream(messages)
-    for chunk in chat.stream(messages) :
+async def base_chat(topic):
+    llm=init_llm("zhipu")
+    chain = prompt | llm
+    async for chunk in chain.astream({"topic": topic}):
+        #print(chunk.content, end="", flush=True)
         yield chunk.content
 
 
-a=base_chat(messages)
-print(list(a))
+
+# 在异步环境中运行
+asyncio.run(base_chat("bear"))
 
 
