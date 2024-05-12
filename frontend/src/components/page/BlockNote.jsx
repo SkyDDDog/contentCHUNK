200ae6fd53fe2ce2a640af1e9e515d3441fd3cd7
林致超 <102146149+1315021555@users.noreply.github.com>
2024-05-12 16:43:10 +0800

    feat: 添加选中的自定义工具栏

--- frontend/src/components/page/BlockNote.jsx
+++ frontend/src/components/page/BlockNote.jsx
@@ -1,29 +1,71 @@
 import '@blocknote/core/fonts/inter.css'
-import { BlockNoteView, useCreateBlockNote } from '@blocknote/react'
+import {
+  useCreateBlockNote,
+  BasicTextStyleButton,
+  BlockTypeSelect,
+  ColorStyleButton,
+  CreateLinkButton,
+  FormattingToolbar,
+  FormattingToolbarController,
+  ImageCaptionButton,
+  NestBlockButton,
+  ReplaceImageButton,
+  TextAlignButton,
+  UnnestBlockButton,
+} from '@blocknote/react'
+import { BlockNoteView } from '@blocknote/mantine'
 import '@blocknote/react/style.css'
+import '@blocknote/mantine/style.css'
 import useMainWidth from '../../utils/useMainWidth'
 import { useSelector } from 'react-redux'
+import { BlueButton } from './editor/toolbar/BlueButton'
+
 export default function App() {
   // Creates a new editor instance
   const editor = useCreateBlockNote({
-    /*  initialContent: [
+    initialContent: [
       {
         type: 'paragraph',
         content: 'Welcome to this demo!',
       },
       {
         type: 'paragraph',
-        content: "You'll see that the text is now blue",
+        content: [
+          {
+            type: 'text',
+            text: 'You can now toggle ',
+            styles: {},
+          },
+          {
+            type: 'text',
+            text: 'blue',
+            styles: { textColor: 'blue', backgroundColor: 'blue' },
+          },
+          {
+            type: 'text',
+            text: ' and ',
+            styles: {},
+          },
+          {
+            type: 'text',
+            text: 'code',
+            styles: { code: true },
+          },
+          {
+            type: 'text',
+            text: ' styles with new buttons in the Formatting Toolbar',
+            styles: {},
+          },
+        ],
       },
       {
         type: 'paragraph',
-        content:
-          "Press the '/' key - the hovered Slash Menu items are also blue",
+        content: 'Select some text to try them out',
       },
       {
         type: 'paragraph',
       },
-    ], */
+    ],
   })
   const leftWidth = useSelector((state) => state.sideNav.sideNavWidth)
   const rightWidth = useSelector((state) => state.chat.boxWidth)
@@ -35,7 +77,63 @@ export default function App() {
       className="editorWrap"
       style={{ width: '100%', height: '100%', maxWidth: mainWidth + 'px' }}
     >
-      <BlockNoteView editor={editor} />
+      <BlockNoteView editor={editor} formattingToolbar={false}>
+        <FormattingToolbarController
+          formattingToolbar={() => (
+            <FormattingToolbar>
+              <BlockTypeSelect key={'blockTypeSelect'} />
+
+              {/* Extra button to toggle blue text & background */}
+              <BlueButton key={'customButton'} />
+
+              <ImageCaptionButton key={'imageCaptionButton'} />
+              <ReplaceImageButton key={'replaceImageButton'} />
+
+              <BasicTextStyleButton
+                basicTextStyle={'bold'}
+                key={'boldStyleButton'}
+              />
+              <BasicTextStyleButton
+                basicTextStyle={'italic'}
+                key={'italicStyleButton'}
+              />
+              <BasicTextStyleButton
+                basicTextStyle={'underline'}
+                key={'underlineStyleButton'}
+              />
+              <BasicTextStyleButton
+                basicTextStyle={'strike'}
+                key={'strikeStyleButton'}
+              />
+              {/* Extra button to toggle code styles */}
+              <BasicTextStyleButton
+                key={'codeStyleButton'}
+                basicTextStyle={'code'}
+              />
+
+              <TextAlignButton
+                textAlignment={'left'}
+                key={'textAlignLeftButton'}
+              />
+              <TextAlignButton
+                textAlignment={'center'}
+                key={'textAlignCenterButton'}
+              />
+              <TextAlignButton
+                textAlignment={'right'}
+                key={'textAlignRightButton'}
+              />
+
+              <ColorStyleButton key={'colorStyleButton'} />
+
+              <NestBlockButton key={'nestBlockButton'} />
+              <UnnestBlockButton key={'unnestBlockButton'} />
+
+              <CreateLinkButton key={'createLinkButton'} />
+            </FormattingToolbar>
+          )}
+        />
+      </BlockNoteView>
     </div>
   )
 }
