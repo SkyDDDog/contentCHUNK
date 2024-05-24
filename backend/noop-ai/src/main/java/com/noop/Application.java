ccb8db6f78ce9676307200d124462ea0b1263f22
SkyDog <362664609@qq.com>
2024-05-24 23:26:31 +0800

    feat: update docker supports & test flux api

---
+++ backend/noop-ai/src/main/java/com/noop/Application.java
@@ -0,0 +1,20 @@
+package com.noop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 主启动类
+ *
+ * @author 天狗
+ * @version 1.0
+ * @since 2024/5/24 22:58
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ backend/noop-ai/src/main/java/com/noop/controller/AiController.java
@@ -0,0 +1,36 @@
+package com.noop.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * AI控制器
+ *
+ * @author 天狗
+ * @version 1.0
+ * @since 2024/5/24 23:15
+ */
+@Tag(name = "AiController", description = "Ai对接控制器")
+@Slf4j
+@CrossOrigin("*")
+@RestController()
+@RequestMapping(value = "/ai")
+@SecurityRequirement(name = "Bearer Authentication")
+public class AiController {
+
+    @Operation(summary = "测试流式返回接口")
+    @GetMapping("test")
+    public Mono<String> test() {
+        return Mono.just("test");
+    }
+
+}
